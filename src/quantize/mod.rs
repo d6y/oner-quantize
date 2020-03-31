d7f62b5fdcf2a64f9c4ea4a0268d2a5724904ab2
Richard Dallaway <richard@dallaway.com>
2020-03-31 13:59:10 +0100

    split when attr and class changes

--- src/quantize/mod.rs
+++ src/quantize/mod.rs
@@ -51,21 +51,31 @@ where
     }
     sorted.ord_subset_sort_by_key(|pair| pair.0);
 
-    // 2. Create a split each time the classification changes
-    let mut split_index = Vec::new(); // Index into `sorted` where the classification changes to a different value.
-    for (prev_index, ((_cur_value, cur_class), (_prev_val, prev_class))) in
+    // 2. Create a (tentative) split each time the attribute value changes and the class value changes
+
+    // Index into `sorted` where the classification changes to a different value.
+    // That is, a value of 1 in split_index means the attribute value at sorted[0] differs from sorted[1].
+    // The split happens between index 0 and 1 in that example.
+    let mut split_index = Vec::new();
+    for (prev_index, ((cur_value, cur_class), (prev_value, prev_class))) in
         sorted.iter().skip(1).zip(sorted.iter()).enumerate()
     {
-        if cur_class != prev_class {
+        if cur_value != prev_value && cur_class != prev_class {
             split_index.push(prev_index + 1);
         }
     }
 
+    // println!("Sorted: {:?}", &sorted);
+    // println!("Split index: {:?}", &split_index);
+    // println!("{:?}", &sorted);
+
     // 3. Remove splits that are too small:
-    let split_trimmed = trim_splits(split_index, small, &sorted);
+    let split_index_trimmed = trim_splits(split_index, small, &sorted);
+    // println!("split_timmed {:?}", &split_index_trimmed);
 
     // 4. Generate distinct intervals from the splits:
-    let intervals: Vec<Interval<A, C>> = intervals_from_splits(split_trimmed, &sorted);
+    let intervals: Vec<Interval<A, C>> = intervals_from_splits(split_index_trimmed, &sorted);
+    // println!("unmerged intervals {:?}", &intervals);
 
     merge_neighbours_with_same_class(&intervals)
 }
