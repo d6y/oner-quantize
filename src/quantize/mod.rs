c820e843f422e760784b602024c9864215448cf9
Richard Dallaway <richard@dallaway.com>
2020-04-01 15:13:03 +0100

    remove unused code

--- src/quantize/mod.rs
+++ src/quantize/mod.rs
@@ -167,18 +167,3 @@ where
 {
     intervals.iter().find(|interval| interval.matches(attribute_value))
 }
-
-/*
-    // 5. Generate a re-mapping table from each value we've seen to a new qualitized value:
-    let interval = |value: &A| merged_intervals.iter().find(|i| i.matches(value));
-
-    let mut remapping: HashMap<&str, String> = HashMap::new();
-
-    let original_string_values = column.iter().map(|(k, _v)| k);
-    let numeric_values = sorted.iter().map(|(k, _v)| k);
-    for (numeric, value) in numeric_values.zip(original_string_values) {
-        if let Some(ival) = interval(*numeric) {
-            remapping.insert(value, ival.show());
-        }
-    }
-*/
